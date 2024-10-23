11c41f79e10ef5bce28cdba9f06b173ce50a3b5a
pliniogim <pliniogimenez@gmail.com>
2024-10-22 22:02:56 -0300

    Initial

---
+++ lib/app/data/services/storage/repository.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:getx_todo_app/app/data/models/task.dart';
+import 'package:getx_todo_app/app/data/providers/task/provider.dart';
+
+class TaskRepository {
+  TaskProvider taskProvider;
+
+  TaskRepository({
+    required this.taskProvider,
+  });
+
+  List<Task> readTasks() => taskProvider.readTasks();
+  void writeTasks(List<Task> tasks) => taskProvider.writeTasks(tasks);
+}
